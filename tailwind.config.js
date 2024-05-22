8227e2ec8ef712ece44426cdd6d64b446b667982
MarcOsorio <marc.osorio.biz@gmail.com>
2024-05-22 15:52:22 -0400

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [],
+  theme: {
+    colors : {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      green: colors.green,
+      blue: colors.blue
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
